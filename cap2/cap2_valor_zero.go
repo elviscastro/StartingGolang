174f111f0173de14e5883d56b3115cb418a8934e
Elvis Castro <ects@discente.ifpe.edu.br>
2022-11-17 10:33:49 -0300

    aula valor zero

--- cap2/cap2_valor_zero.go
+++ cap2/cap2_valor_zero.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var x int = 0
+var y float64 = 0
+var z bool = false
+var s string = ""
 
 func main() {
 
-	fmt.Println("Hello")
+	// imprimindo cada tipo com seus 'zeros'
+
+	fmt.Printf("x: %v (%T)\n", x, x)
+	fmt.Printf("y: %.1f (%T)\n", y, y)
+	fmt.Printf("z: %v (%T)\n", z, z)
+	fmt.Printf("s: %v (%T)", s, s)
 
 }
